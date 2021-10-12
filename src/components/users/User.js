4f22bea7a38fb6a000a06f1a9958a11232088207
Roby Gupta <robygupta98@gmail.com>
2021-10-13 02:25:26 +0530

    Updated project

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 
@@ -13,7 +13,7 @@ const User = () => {
   const { id } = useParams();
   useEffect(() => {
     loadUser();
-  }, []);
+}, []);
   const loadUser = async () => {
     const res = await axios.get(`http://localhost:3002/users/${id}`);
     setUser(res.data);
